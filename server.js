62fa8c9d0ac1a627ffe27ffa1e1b2ec1778edc0b
kennybaquolmusic@gmail.com <kennybaquolmusic@gmail.com>
2022-06-07 10:38:23 -0400

    Established database connection + created models/schema

--- server.js
+++ server.js
@@ -5,6 +5,40 @@ require("dotenv").config()
 const express = require("express")
 const morgan = require('morgan')
 const methodOverride = require('method-override')
-const req = require('express/lib/request')
 const mongoose = require('mongoose')
 const path = require('path')
+
+/////////////////////////////////////////////
+// Database Connection
+/////////////////////////////////////////////
+// Setup inputs for our connect function
+const DATABASE_URL = process.env.DATABASE_URL;
+const CONFIG = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+};
+
+// Establish Connection
+mongoose.connect(DATABASE_URL, CONFIG);
+
+// Events for when connection opens/disconnects/errors
+mongoose.connection
+    .on("open", () => console.log("Connected to Mongoose"))
+    .on("close", () => console.log("Disconnected from Mongoose"))
+    .on("error", (error) => console.log(error))
+
+////////////////////////////////////////////////
+// Our Models
+////////////////////////////////////////////////
+// pull schema and model from mongoose
+const { Schema, model } = mongoose
+
+// make fruits schema
+const fruitsSchema = new Schema({
+    name: String,
+    color: String,
+    readyToEat: Boolean
+})
+
+// make fruit model
+const Fruit = model("Fruit", fruitsSchema)
